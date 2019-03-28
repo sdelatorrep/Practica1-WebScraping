37fcc227845d68b33a7754e2a91c848347b66aef
gervasiocuenca <48950660+gervasiocuenca@users.noreply.github.com>
2019-03-28 20:31:01 +0100

    Create PaisesScrapper.py

---
+++ PaisesScrapper.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+import requests
+
+class PaisesScrapper():
+
+    def __init__(self):
+        self.url = "https://knoema.es/atlas"
+
+    def __Baja_HTML(self, url):
+        pagina = requests.get(url)
+        estructura = BeautifulSoup(pagina.content, "html.parser")
+        return estructura
+
+    def __lista_paises_links(self, estructura):
+        estructura.findAll('div')
+        midiv = estructura.find_all('div',{'class':'container'})
+        m = midiv[0]
+        lista_paises=[]
+        for link in m.find_all('a'):
+            pais = []
+            pais.append(link.string)
+            pais.append(url+link.get('href'))
+            lista_paises.append(pais)
+        return lista_paises
