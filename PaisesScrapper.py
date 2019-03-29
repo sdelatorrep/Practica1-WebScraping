1416c59b1a9c49780d9a7707367f152002894495
gervasiocuenca <48950660+gervasiocuenca@users.noreply.github.com>
2019-03-29 10:25:58 +0100

    Update PaisesScrapper.py

--- PaisesScrapper.py
+++ PaisesScrapper.py
@@ -40,7 +40,7 @@ class PaisesScrapper():
     def Inicio_Prueba(self):
         lista = self.__lista_paises_links()
         print(lista)a_paises_links(self):
-        esructura = self.__Baja_HTML(self.url)
+        estructura = self.__Baja_HTML(self.url)
         estructura.findAll('div')
         midiv = estructura.find_all('div',{'class':'container'})
         m = midiv[0]
@@ -48,7 +48,7 @@ class PaisesScrapper():
         for link in m.find_all('a'):
             pais = []
             pais.append(link.string)
-            pais.append(url+link.get('href'))
+            pais.append(self.url+link.get('href'))
             lista_paises.append(pais)
         return lista_paises
     
