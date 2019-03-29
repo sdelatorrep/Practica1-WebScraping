66a3bd6c5d00a220c5f088df8fb309ea09e0d7ac
Sabela <sabela.delatorre@crg.eu>
2019-03-29 18:52:12 +0100

    Obtener otros indicadores que no están en la página principal

--- paises_scraper.py
+++ paises_scraper.py
@@ -3,6 +3,10 @@
 
 from bs4 import BeautifulSoup
 import requests
+import logging
+
+
+logging.getLogger()
 
 
 class PaisesScraper:
@@ -19,14 +23,14 @@ class PaisesScraper:
         self.indicadores.append("Tasa de desempleo")
         self.indicadores.append("Importaciones de bienes y servicios")
         self.indicadores.append("Exportaciones de bienes y servicios")
-        self.indicadores.append("Demografia")
+        # self.indicadores.append("Demografia")
         self.indicadores.append("Población")
         self.indicadores.append("Tasa de natalidad")
         self.indicadores.append("Tasa de mortalidad")
         self.indicadores.append("Tasa de fecundidad")
         self.indicadores.append("Esperanza de vida al nacer")
         self.indicadores.append("Relación empleo/población")
-        self.indicadores.append("Uso Tierra")
+        # self.indicadores.append("Uso Tierra")
         self.indicadores.append("Terrenos agrícolas (km2)")
         self.indicadores.append("Zona forestal")
         self.indicadores.append("Mortalidad infantil")
@@ -39,9 +43,10 @@ class PaisesScraper:
         self.indicadores.append("Ingresos por turismo")
         self.indicadores.append("Gasto en investigación y desarrollo, % del PIB")
         self.indicadores.append("Índice de Desarrollo Humano")
-        self.indicadores.append("Felicidad")
+        self.indicadores.append("Happiness")
         self.indicadores.append("Usuarios de internet, % de la población")
-        self.indicadores.append("Accidentes con heridos (tráfico)")
+        self.indicadores.append("Accidentes con heridos")
+        # logging.debug("Total indicadores: {}".format(len(self.indicadores)))
 
     @staticmethod
     def baja_html(url):
@@ -50,32 +55,98 @@ class PaisesScraper:
         return estructura
 
     def lista_paises_links(self):
+        """
+        Obtiene la URL de cada país
+        :return:
+        """
         estructura = self.baja_html(self.url)
         midiv = estructura.find_all("div", {"class": "container"})
         lista_paises = list()
         for link in midiv[0].find_all("a"):
             pais = list()
             pais.append(link.string)
-            pais.append(self.raiz+link.get("href"))
+            pais.append(self.raiz + link.get("href"))
+            # logging.debug("Pais: {}".format(pais))
             lista_paises.append(pais)
+
         return lista_paises
     
-    def lista_indicadores_links(self, url):
+    def buscar_links_indicadores(self, url):
+        """
+        Obtiene la URL de cada indicador
+        :param url:
+        :return:
+        """
         estructura = self.baja_html(url)
         midiv = estructura.find_all("a", string=self.indicadores)
         lista_links = list()
         for link in midiv:
-            indicador = list()
-            indicador.append(link.string)
-            indicador.append(link.get("href"))
-            lista_links.append(indicador)
+            lista_links.append((link.string, link.get("href")))
+
+        indicadores_no_encontrados = self.revisar_indicadores(lista_links)
+        if len(indicadores_no_encontrados) > 0:
+            self.buscar_mas_indicadores(indicadores_no_encontrados, lista_links, url)
         return lista_links
+
+    def revisar_indicadores(self, lista_links):
+        """
+        Revisa si están todos los indicadores en lista_links.
+        :param lista_links:
+        :return:
+        """
+        indicadores_no_encontrados = list()
+        if len(lista_links) != (len(self.indicadores)):
+            logging.error("Faltan indicadores! Encontrados: {}, debería haber: {}".format(len(lista_links),
+                                                                                          len(self.indicadores)))
+            for ind in self.indicadores:
+                encontrado = False
+                for links in lista_links:
+                    if ind == links[0]:
+                        encontrado = True
+                        break
+                if not encontrado:
+                    indicadores_no_encontrados.append(ind)
+                    logging.error("Indicador no encontrado: {}".format(ind))
+        return indicadores_no_encontrados
+
+    def buscar_mas_indicadores(self, indicadores_no_encontrados, lista_links, url):
+        """
+        Algunos indicadores no aparecen en la página principal y hay que ir a buscarlos a .../topics/...
+        :param indicadores_no_encontrados:
+        :param lista_links:
+        :param url:
+        :return:
+        """
+        estructura = self.baja_html(url)
+        todos_tag_a = estructura.find_all("a")
+        for tag in todos_tag_a:
+            href = tag.get('href', None)
+            if href is not None and "topics" in href and href.startswith('https'):
+                # logging.debug("Tema: {}".format(href))
+                estructura_tema = self.baja_html(href)
+                mi_div = estructura_tema.find_all("a", string=self.indicadores)
+                for link in mi_div:
+                    encontrado = False
+                    # Comprobamos que no se haya añadido ya
+                    for links in lista_links:
+                        if link.string == links[0]:
+                            encontrado = True
+                            break
+                    if not encontrado:
+                        # Añadir indicador
+                        logging.debug("Añadir indicador: {}".format(link.string))
+                        lista_links.append((link.string, link.get("href")))
+
+        indicadores_no_encontrados = self.revisar_indicadores(lista_links)
+        if len(indicadores_no_encontrados) > 0:
+            logging.error("Faltan indicadores! {}".format(indicadores_no_encontrados))
+            exit(1)
 
     def inicio_prueba(self):
         paises = self.lista_paises_links()
         for p in paises:
             url = p[1]
-            indicadores = self.lista_indicadores_links(url)
-            print(url)
-            print(indicadores)
+            indicadores = self.buscar_links_indicadores(url)
+            logging.debug("URL: {}".format(url))
+            logging.debug("Indicadores: {}".format(indicadores))
             break
